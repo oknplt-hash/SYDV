48c1659d866a8c478457be47873c996c8a5301b1
okan <oknplt@gmail.com>
2026-01-15 17:10:46 +0300

    Debug: Add database connection logging

--- api/index.js
+++ api/index.js
@@ -16,6 +16,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Database Connection
+console.log('Database connecting to:', process.env.DATABASE_URL ? process.env.DATABASE_URL.split('@')[1] : 'UNDEFINED');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
